3c6259b4db91a7571cbbdbc7d5eb7082557b4517
Sanqui <gsanky@gmail.com>
2016-07-11 12:24:00 +0200

    Work on styling and pagination
    (This commit was backdated from 2016-08-15)

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, abort, g, Blueprint
 import werkzeug.utils
 import yaml
+form enum import Enum
 
 app = Flask(__name__)
 
@@ -12,6 +13,11 @@ try:
 except werkzeug.utils.ImportStringError:
     print("No config present: using example config.")
 
+class Location(Enum):
+    single = 0
+    left = 1
+    right = 2
+
 comic_ids = yaml.load(open(app.config['DATA_DIR']+"comics.yaml"))
 comics = {}
 
@@ -20,11 +26,29 @@ for comic_id in comic_ids:
     blueprint = Blueprint(comic_id, __name__, static_url_path='/{}/img'.format(comic_id), static_folder=app.config['DATA_DIR']+comic_id+"/img")
     app.register_blueprint(blueprint)
 
+def get_page_by_id(comic, page_id):
+    page = None
+    for i, p in enumerate(comic['pages']):
+        if page_id == p['id']:
+            pagei = i
+            page = p
+            break
+    if page:
+        return pagei, page
+    else:
+        return None
+
+def get_page_physical_location(comic, page_id):
+    current = None
+    for i, p in enumerate(comic['pages']):
+        if comic['pages'] == 
+
 @app.before_request
 def before_request():
     g.comic_ids = comic_ids
     g.comics = comics
     g.sitename = app.config['SITENAME']
+    g.debug = app.config['DEBUG']
 
 @app.route('/')
 def index():
@@ -40,18 +64,18 @@ def comic(comic_id):
     return render_template("comic.html", comic=comic)
 
 @app.route('/<comic_id>/<page_id>')
-def page(comic_id, page_id):
+@app.route('/<comic_id>/<page_id>+<page2_id>')
+def page(comic_id, page_id, page2_id=None):
     if comic_id not in comics: abort(404)
     
     comic = comics[comic_id]
     
-    pagei = None
-    page = None
-    for i, p in enumerate(comic['pages']):
-        if page_id == p['id']:
-            pagei = i
-            page = p
-            break
+    double = False
+    pages = []
+    pages.append(get_page_by_id(comic, page_id)
+    if page2_id:
+        pages.append(get_page_by_id(comic, page2_id))
+        double = True
     
     if not page: abort(404)
     
