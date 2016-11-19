2abe7ad908636edc29fe39a88b66478802ff17e9
Sanqui <gsanky@gmail.com>
2016-11-19 12:23:06 +0100

    Port over to attrs

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from enum import Enum
+from pprint import pprint
 from flask import Flask, render_template, abort, g, Blueprint
 import werkzeug.utils
 import yaml
-from enum import Enum
+from attr import attrs, attrib
 
 app = Flask(__name__)
 
@@ -18,18 +20,50 @@ class Location(Enum):
     left = 1
     right = 2
 
+@attrs(slots=True)
+class Page(object):
+    id = attrib()
+    name = attrib(default=None)
+    date = attrib(default=None)
+    comment = attrib(default=None)
+    position = attrib(default=None)
+
+@attrs(slots=True)
+class Comic(object):
+    id = attrib()
+    name = attrib()
+    author = attrib()
+    language = attrib()
+    date = attrib()
+    comment = attrib()
+    format = attrib()
+    page_layout = attrib()
+    page_names = attrib()
+    page_icons = attrib()
+    pages = attrib()
+
+
 comic_ids = yaml.load(open(app.config['DATA_DIR']+"comics.yaml"))
 comics = {}
 
 for comic_id in comic_ids:
-    comics[comic_id] = yaml.load(open(app.config['DATA_DIR']+comic_id+"/comic.yaml"))
+    comic = yaml.load(open(app.config['DATA_DIR']+comic_id+"/comic.yaml"))
+    comic['pages'] = [Page(**p) for p in comic['pages']]
+    comics[comic_id] = Comic(**comic)
+    
     blueprint = Blueprint(comic_id, __name__, static_url_path='/{}/img'.format(comic_id), static_folder=app.config['DATA_DIR']+comic_id+"/img")
     app.register_blueprint(blueprint)
 
+print("Loaded the comics:")
+for comic_id, comic in comics.items():
+    print(" - {} ({} pages)".format(comic.id, len(comic.pages)))
+
+print()
+
 def get_page_by_id(comic, page_id):
     page = None
-    for i, p in enumerate(comic['pages']):
-        if page_id == p['id']:
+    for i, p in enumerate(comic.pages):
+        if page_id == p.id:
             pagei = i
             page = p
             break
@@ -37,11 +71,6 @@ def get_page_by_id(comic, page_id):
         return pagei, page
     else:
         return None
-
-def get_page_physical_location(comic, page_id):
-    current = None
-    #for i, p in enumerate(comic['pages']):
-    #    if comic['pages'] == 
 
 @app.before_request
 def before_request():
@@ -70,20 +99,12 @@ def page(comic_id, page_id, page2_id=None):
     
     comic = comics[comic_id]
     
-    double = False
-    pages = []
-    pages.append(get_page_by_id(comic, page_id))
-    if page2_id:
-        pages.append(get_page_by_id(comic, page2_id))
-        double = True
-    
-    page = pages[0][1]
-    pagei = pages[0][0]
+    pagei, page = get_page_by_id(comic, page_id)
     
     if not page: abort(404)
     
-    prev = comic['pages'][pagei-1] if pagei > 0 else None
-    next = comic['pages'][pagei+1] if pagei < len(comic['pages'])-1 else None
+    prev = comic.pages[pagei-1] if pagei > 0 else None
+    next = comic.pages[pagei+1] if pagei < len(comic.pages)-1 else None
     
     return render_template("page.html", comic=comic, page=page, prev=prev, next=next)
 
